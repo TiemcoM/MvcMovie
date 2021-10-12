5be49cae905ef09ce48e3af62ffcb39ba384f07b
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-10-12 14:51:20 +0200

    Add project files.

---
+++ MvcMovieMeernik/Data/MvcMovieMeernikContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieMeernik.Models;
+
+namespace MvcMovieMeernik.Data
+{
+    public class MvcMovieMeernikContext : DbContext
+    {
+        public MvcMovieMeernikContext (DbContextOptions<MvcMovieMeernikContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovieMeernik.Models.Movie> Movie { get; set; }
+    }
+}
